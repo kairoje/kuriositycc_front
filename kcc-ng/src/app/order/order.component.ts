fb1620566e4ddf12fb2243d1b23e0bc88d832ce9
kairoje <129036688+kairoje@users.noreply.github.com>
2023-10-27 07:36:09 -0700

    styling edits

--- kcc-ng/src/app/order/order.component.ts
+++ kcc-ng/src/app/order/order.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  user: any;
+  product: any;
 
+  getUserDetails(userId: string) {
+    return this.http.get(`/api/auth/users/${userId}`);
+  }
+
+  getProductDetails(productId: string) {
+    return this.http.get(`/api/products/${productId}`);
+  }
+  
+
+  constructor(private apiService: ApiService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const userId = 'user-id';
+    this.apiService.getUserDetails(userId).subscribe((userData: any) => {
+      this.user = userData;
+    });
+
+
+    const productId = 'product-id';
+    this.apiService.getProductDetails(productId).subscribe((productData: any) => {
+      this.product = productData;
+    });
+  }
 }
